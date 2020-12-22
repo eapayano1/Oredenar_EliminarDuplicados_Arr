38d9052dbbb2471db6f027c772e6f19647e7a83e
Emiko47 <emilpayano70@gmail.com>
2020-12-22 19:22:43 -0400

    SegundoTesteo

--- ordeli_arr.py
+++ ordeli_arr.py
@@ -1,7 +1,18 @@
 def Sort_Array(arr):
-    n=len(arr)
-    for i in range(n-1):
-        for x in range(n-1-i):
-            if arr[x]>arr[x+1]:
+    longitud=len(arr)
+    for i in range(longitud-1):
+        for x in range(longitud-1-i):
+            if arr[x]>arr[x+1]:##Si el elemento en el que nos encontramos es mayor al elemento siguiente entonces intercambiamos sus valores.
                 arr[x],arr[x+1]=arr[x+1],arr[x]
     return arr
+
+def EliminateDuplicate(arr):
+    longitud=len(arr)
+    myarr=[]
+    Sort_Array(arr)
+    for i in range(longitud-1):
+        if arr[i]!=arr[i+1]:##Si el elemento en el que nos encontramos es diferente al elemento siguiente entonces guardamos el elemento en el que nos encontramos.
+            myarr.append(arr[i])
+    myarr.append(arr[longitud-1])
+
+    return myarr
--- test_OrdEliArray.py
+++ test_OrdEliArray.py
@@ -6,3 +6,8 @@ class Test_OrdEliArray(unittest.TestCase):
         actual=Sort_Array([1,2,2,5,5,3,4])
         expected=[1,2,2,3,4,5,5]
         self.assertEqual(expected, actual)
+    
+    def test_EliminateDuplicate(self):
+        actual=EliminateDuplicate([1,2,2,5,5,3,4])
+        expected=[1,2,3,4,5]
+        self.assertEqual(expected, actual)
