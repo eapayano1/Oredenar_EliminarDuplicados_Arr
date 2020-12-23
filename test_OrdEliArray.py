90d9e310e850830d12876ee6800c0e0905f74ff7
Emiko47 <emilpayano70@gmail.com>
2020-12-23 00:22:46 -0400

    PrimerTesteo1

---
+++ test_OrdEliArray.py
@@ -0,0 +1,8 @@
+from OrdEli_arr import *
+import unittest
+
+class Test_OrdEliArray(unittest.TestCase):
+    def test_SortArray(self):
+        actual=Sort_Array([1,2,2,5,5,3,4])
+        expected=[1,2,2,3,4,5,5]
+        self.assertEqual(expected, actual)
